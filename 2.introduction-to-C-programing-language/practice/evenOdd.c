ce70700aec52d025ec1938105c79da7b5fb76187
FahimMontasir <fahimmontasirshakil@gmail.com>
2022-04-21 16:10:09 +0600

    add: lab class 2

---
+++ 2.introduction-to-C-programing-language/practice/evenOdd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    int num;
+    scanf("%d", &num);
+
+    if(num%2 == 0) {
+        printf("The number is even");
+    }
+    else {
+        printf("The number is odd");
+    }
+}
---
+++ 2.introduction-to-C-programing-language/practice/minMax.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+    int a = 20;
+    int b = 15;
+
+    int max_ab = a > b ? a : b ;
+
+    printf("%d", max_ab);
+}
