ce6838c3c29ce776aca3adc27ec1457ef21065ae
FahimMontasir <fahimmontasirshakil@gmail.com>
2023-07-05 00:06:02 +0600

    [learnerd] linked list with tail pointer

--- DSA-JS/ds/linkedList.js
+++ DSA-JS/ds/linkedList.js
@@ -18,7 +18,7 @@ class LinkedList {
   getSize() {
     return this.size;
   }
-
+  // O(1)
   prepend(value) {
     const node = new Node(value);
     if (this.isEmpty()) {
@@ -29,6 +29,125 @@ class LinkedList {
     }
     this.size++;
   }
+  // O(n)
+  append(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      let prev = this.head;
+      while (prev.next) {
+        prev = prev.next;
+      }
+      prev.next = node;
+    }
+
+    this.size++;
+  }
+
+  insert(value, index) {
+    if (index < 0 || index > this.size) {
+      console.log("Invalid insert index");
+      return;
+    }
+    if (index === 0) {
+      this.prepend(value);
+    } else {
+      const node = new Node(value);
+      let prev = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        prev = prev.next;
+      }
+      node.next = prev.next;
+      prev.next = node;
+      this.size++;
+    }
+  }
+  //O(n)
+  removeFrom(index) {
+    if (index < 0 || index >= this.size) {
+      return null;
+    }
+    let removeNode;
+    if (index === 0) {
+      removeNode = this.head;
+      this.head = this.head.next;
+    } else {
+      let prev = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        prev = prev.next;
+      }
+      removeNode = prev.next;
+      prev.next = removeNode.next;
+    }
+    this.size++;
+    return removeNode.value;
+  }
+  //O(n)
+  removeValue(value) {
+    if (this.isEmpty()) {
+      return null;
+    }
+    if (this.head.value === value) {
+      this.head = this.head.next;
+      this.size--;
+      return value;
+    } else {
+      let prev = this.head;
+      while (prev.next && prev.next.value !== value) {
+        prev = prev.next;
+      }
+      if (prev.next) {
+        const removedNode = prev.next;
+        prev.next = removedNode.next;
+        this.size--;
+        return value;
+      }
+      return null;
+    }
+  }
+
+  search(value) {
+    if (this.isEmpty()) {
+      return -1;
+    }
+    let i = 0;
+    let curr = this.head;
+    while (curr) {
+      if (curr.value === value) {
+        return i;
+      }
+      curr = curr.next;
+      i++;
+    }
+    return -1;
+  }
+
+  reverse() {
+    let prev = null;
+    let curr = this.head;
+    while (curr) {
+      let next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+    this.head = prev;
+  }
+
+  print() {
+    if (this.isEmpty()) {
+      console.log("List is empty");
+    } else {
+      let curr = this.head;
+      let listValues = "";
+      while (curr) {
+        listValues += `${curr.value} `;
+        curr = curr.next;
+      }
+      console.log(listValues);
+    }
+  }
 }
 
 const list = new LinkedList();
@@ -37,3 +156,19 @@ console.log("List size:", list.getSize());
 
 list.prepend(10);
 list.prepend(20);
+list.print();
+
+list.prepend(30);
+list.append(5);
+list.insert(1, 0);
+list.insert(70, 2);
+
+list.print();
+console.log(list.removeFrom(0));
+console.log(list.removeFrom(2));
+list.print();
+list.removeValue(5);
+list.print();
+console.log(list.search(70));
+list.reverse();
+list.print();
