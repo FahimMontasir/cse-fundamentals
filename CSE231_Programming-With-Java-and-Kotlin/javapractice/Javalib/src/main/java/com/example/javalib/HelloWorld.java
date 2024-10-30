74c963bf55a99cea06e9cd40b39644376cbd7b68
FahimMontasir <fahimmontasirshakil@gmail.com>
2024-10-30 19:06:32 +0600

    feat: Enhance HelloWorld program to display lucky numbers

--- CSE231_Programming-With-Java-and-Kotlin/javapractice/Javalib/src/main/java/com/example/javalib/HelloWorld.java
+++ CSE231_Programming-With-Java-and-Kotlin/javapractice/Javalib/src/main/java/com/example/javalib/HelloWorld.java
@@ -2,6 +2,14 @@ package com.example.javalib;
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.print("hello world");
+        System.out.print("hello world \n");
+
+        int[] luckyNumbers = {10, 20, 50};
+
+        for (int i : luckyNumbers) {
+            System.out.println(i);
+        }
+
+        System.out.println(luckyNumbers[0]);
     }
 }
